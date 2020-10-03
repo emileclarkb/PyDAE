d5c713d843557c8315ec6e9edb1d7790097c8791
emileclarkb <nordicdsgn@gmail.com>
2020-10-03 13:11:32 +1000

    initial commit

---
+++ cmds.bat
@@ -0,0 +1,7 @@
+echo "# PyDAE" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/emileclarkb/PyDAE.git
+git push -u origin main
